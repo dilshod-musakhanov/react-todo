3a227c06dde8b353a33476304fcca030ef059bb7
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-02-11 12:19:46 +0400

    use state to mark todo task as done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,129 +2,25 @@ import React, {Component} from "react";
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+    state = {
+        done: false
+    };
 
     onLabelClick = () => {
-        console.log(`Done:${this.props.label}`)
-    }
+        this.setState({
+            done: true
+        });
+    };
 
     render() {
 
         const {label, important = false} = this.props;
+        const {done} = this.state;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
-
-        return (
-            <span className="todo-list-item">
-                <span
-                    className="todo-list-item-label"
-                    style={style}
-                    onClick={this.onLabelClick}>
-                    {label}
-                </span>
-
-                <button
-                    type="button"
-                    className="btn btn-outline-success btn-sm float-right">
-                    <i className="fa fa-exclamation" />
-                </button>
-
-                <button
-                    type="button"
-                    className="btn btn-outline-danger btn-sm float-right">
-                    <i className="fa fa-trash-o" />
-                </button>
-            </span>
-        );
-
-    }
-}
-
-
-/*
-
-//on below when we pass onLabelClick function to onClick event then
-//onLabelClick function lose its reference to this
-//which will result in error message "Uncaught TypeError: this is undefined"
-
-export default class TodoListItem extends Component {
-
-    onLabelClick(){console.log(`Done:${this.props.label}`)};
-
-    render() {
-
-        const {label, important = false} = this.props;
-
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
-
-        return (
-            <span className="todo-list-item">
-                <span
-                    className="todo-list-item-label"
-                    style={style}
-                    onClick={this.onLabelClick}>
-                    {label}
-                </span>
-
-                <button
-                    type="button"
-                    className="btn btn-outline-success btn-sm float-right">
-                    <i className="fa fa-exclamation" />
-                </button>
-
-                <button
-                    type="button"
-                    className="btn btn-outline-danger btn-sm float-right">
-                    <i className="fa fa-trash-o" />
-                </button>
-            </span>
-        );
-
-    }
-}
-
-//one of the ways to to keep reference to this is using bind
-
-                <span
-                    className="todo-list-item-label"
-                    style={style}
-                    onClick={this.onLabelClick.bind(this)}>
-                    {label}
-                </span>
-
-
-//another better way is to create onLabelClick in the constructor
-//by doing in this way we create function not on the prototype of the class but
-//on the object itself with arrow function to keep reference to this
-
-
-                    constructor() {
-                        super();
-                        this.onLabelClick = () => {
-                            console.log(`Done:${this.props.label}`)
-                        }
-                    }
-
-
-
-export default class TodoListItem extends Component {
-
-    constructor() {
-        super();
-        this.onLabelClick = () => {
-            console.log(`Done:${this.props.label}`)
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
         }
-    }
-
-
-    render() {
-
-        const {label, important = false} = this.props;
 
         const style = {
             color: important ? 'steelblue' : 'black',
@@ -132,7 +28,7 @@ export default class TodoListItem extends Component {
         };
 
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
                 <span
                     className="todo-list-item-label"
                     style={style}
@@ -156,54 +52,3 @@ export default class TodoListItem extends Component {
 
     }
 }
-
-
-//there is also another way to solve this issue
-
-export default class TodoListItem extends Component {
-
-    onLabelClick = () => {
-        console.log(`Done:${this.props.label}`)
-    }
-
-    render() {
-
-        const {label, important = false} = this.props;
-
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
-
-        return (
-            <span className="todo-list-item">
-                <span
-                    className="todo-list-item-label"
-                    style={style}
-                    onClick={this.onLabelClick}>
-                    {label}
-                </span>
-
-                <button
-                    type="button"
-                    className="btn btn-outline-success btn-sm float-right">
-                    <i className="fa fa-exclamation" />
-                </button>
-
-                <button
-                    type="button"
-                    className="btn btn-outline-danger btn-sm float-right">
-                    <i className="fa fa-trash-o" />
-                </button>
-            </span>
-        );
-
-    }
-}
-
-
-
-
-
-
- */
