2fc45dbd4bf0855edd3cd24480b73dbeae90215b
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-02-13 03:28:48 +0400

    refactoring by creatting one toggle function for done and important

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -55,40 +55,41 @@ export default class App extends Component {
         })
     }
 
+    toggleProperty(arr, id, propName) {
+        const idx = arr.findIndex((el) => el.id === id);
+        
+        const oldItem = arr[idx];
+        const newItem = {...oldItem, [propName]: !oldItem[propName]};
+
+        return [
+            ...arr.slice(0, idx),
+            newItem,
+            ...arr.slice(idx + 1)
+        ];
+    }
+
+
     onToggleDone = (id) => {
         this.setState(({todoData})=> {
-            const idx = todoData.findIndex((el) => el.id === id);
-
-            //1.update obj
-            const oldItem = todoData[idx];
-            const newItem = {...oldItem, done: !oldItem.done};
-
-            //2.construct new array
-            const newArray = [
-                ...todoData.slice(0, idx),
-                newItem,
-                ...todoData.slice(idx + 1)
-            ];
             return {
-                todoData: newArray
+                todoData: this.toggleProperty(todoData, id, 'done')
             };
         })
     }
     onToggleImportant = (id) => {
-        console.log('toggle done', id);
+        this.setState(({todoData})=> {
+            return {
+                todoData: this.toggleProperty(todoData, id, 'important')
+            };
+        })
     }
 
     render() {
 
-        /*
-        in order not to repeat this.state.todoData many times
-        we create separate variable for it
         const {todoData} = this.state;
-         */
-        const {todoData} = this.state;
-
         const doneCount = todoData.filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
+
         return (
             <div className="todo-app">
                 <AppHeader toDo={todoCount} done={doneCount}/>
